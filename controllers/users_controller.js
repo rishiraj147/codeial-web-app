386518910d1c4a212ddbe03249262db279783c3f
RishiKumar <rishiraj147r@gmail.com>
2023-07-14 12:50:03 +0530

    add controller and router for users-profile

---
+++ controllers/users_controller.js
@@ -0,0 +1,3 @@
+module.exports.profile=function(req,res){
+    res.end('<h1>Wellcome to our profile! </h1>')
+}
--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ const router=express.Router();
 console.log('router is loaded');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
 
 module.exports=router;
